64ca9c816d59ca99dfcf62c9416842debb86421c
Roberto Albanesi <beto0607@gmail.com>
2023-09-13 09:57:27 +0200

    Add git_configs file

--- aliases/shell_aliases.sh
+++ aliases/shell_aliases.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 export STUDIO_PATH="$HOME/Studio";
+export COMPOSEINFRA_PATH="$HOME/Bannerflow/ComposeInfra";
 
 
 #alias vim="nvim"
@@ -11,7 +12,9 @@ alias lint="npm run lint"
 # STUDIO
 # -- app 
 alias npm_watch="cd $STUDIO_PATH; npm run watch"
-alias npm_start="cd $STUDIO_PATH;sleep 45 && npm run start"
+alias npm_start="cd $STUDIO_PATH; sleep 45 && npm run start"
+alias npm_watch_dev="cd $STUDIO_PATH; npm run watch:app:remote-dev"
+alias npm_start_dev="cd $STUDIO_PATH;export ENABLE_SSL=true; sleep 45 && npm run start"
 alias npm_build_scripts="cd $STUDIO_PATH;npm run build:scripts -- --watch"
 alias npm_acg="cd $STUDIO_PATH;npm run start:acg"
 alias npm_tests="cd $STUDIO_PATH;npm run test:units"
@@ -22,6 +25,9 @@ alias acg_build_start="cd $STUDIO_PATH;npm run build:acg+scripts && npm run star
 alias acg_run="cd $STUDIO_PATH;npm run start:acg"
 # -- test
 alias npm_test_ng="npm run test:ng -- --watch --silent=false"
+
+# ComposeInfra
+alias gateway_up="cd $COMPOSEINFRA_PATH; docker compose up -d gateway; cd -"
 
 # VS-CODE
 alias code.="code ."
